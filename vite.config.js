80e4a72723b347245242ec16d22c673b2c4edda9
Vandana-CN <vandanacn99@gmail.com>
2025-09-07 17:28:11 -0400

    Home fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Vandana/',  
+  define: {
+    BASE: 'import.meta.env.BASE_URL' // <- make BASE always available
+  },
+
 })
