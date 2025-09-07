d3347ec49d6bb107fbd6ef4c653d194c6fcf1254
Vandana-CN <vandanacn99@gmail.com>
2025-09-07 17:34:24 -0400

    Repo name change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Vandana/',  
+  base: '/Portfolio/',  
   define: {
     BASE: 'import.meta.env.BASE_URL' // <- make BASE always available
   },
